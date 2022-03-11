a0c03080bdf0e1c987c58a3045f695635e51171e
Justin747 <justin747@gmail.com>
2022-03-10 20:46:08 -0800

    Initial Commit

---
+++ Finance App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Finance App
+//
+//  Created by Justin747 on 3/10/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Finance App/Finance_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Finance_AppApp.swift
+//  Finance App
+//
+//  Created by Justin747 on 3/10/22.
+//
+
+import SwiftUI
+
+@main
+struct Finance_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
