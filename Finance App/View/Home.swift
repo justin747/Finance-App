8de3ef7f3e55929b9cab2ced81d806547ccc44e2
Justin747 <justin747@gmail.com>
2022-04-13 14:16:34 -0700

    Lazy Update
    Lazy Update

--- Finance App/View/Home.swift
+++ Finance App/View/Home.swift
@@ -22,7 +22,7 @@ struct Home: View {
             
             if !animatedStates[1] {
             RoundedRectangle(cornerRadius: animatedStates[0] ? 30 : 0, style: .continuous)
-                .fill(Color("Purple"))
+                .fill(Color("Blue"))
                 .matchedGeometryEffect(id: "DATEVIEW", in: animation)
                 .ignoresSafeArea()
             }
